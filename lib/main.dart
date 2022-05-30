a0336250f1a2e7fcc583b7dc0ceaeff81a2b527e
Дмитрий Селезнев <Selesnjow@gmail.com>
2022-05-31 02:34:58 +1000

    added: github icons, widgets: [urllaunch, textbutton], app theme, and etc.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'routes/info_page/InfoPage.dart';
+import 'themes/CustomTheme.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,35 +13,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: const Center(
-
-      ),
+      title: 'Swap',
+      theme: CustomTheme.darkTheme,
+      home: const InfoPage(title: 'About'),
     );
   }
 }
---
+++ lib/themes/CustomColors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+//Theme colors
+const Color primaryColor = Color(0xFF262626);
+const Color accentColor = Color(0xFFF25C05);
+const Color secondaryColor = Color(0xFFF24405);
+const Color lightColor = Color(0xFFF2F2F2);
